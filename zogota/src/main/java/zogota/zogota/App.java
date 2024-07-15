ce559c7b41677c65be0005abbef1cec6c2de447c
adoumbouya53 <adoumbouya53@yahoo.com>
2024-07-15 23:23:01 +0200

    Initial commit

---
+++ zogota/src/main/java/zogota/zogota/App.java
@@ -0,0 +1,13 @@
+package zogota.zogota;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zogota/src/test/java/zogota/zogota/AppTest.java
@@ -0,0 +1,20 @@
+package zogota.zogota;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
